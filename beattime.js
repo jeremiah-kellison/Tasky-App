30870b4af39359a152a3b73ffa2c8e2fef99b54a
jeremiah-kellison <kellisonjj@gmail.com>
2024-05-17 14:49:22 -0700

    Added beatTime

---
+++ beattime.js
@@ -0,0 +1,12 @@
+function startTime() {
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var tzoff = 60 + d.getTimezoneOffset();
+    var time = '@' + ('000'+Math.floor((h * 3600 + (m + tzoff) * 60 + s) / 86.4) % 1000).slice(-3);
+
+    const timeElement = document.getElementById('time'); 
+    timeElement.innerText = time; 
+}
+startTime();
